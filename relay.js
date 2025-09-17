aa3dd39396eef744aa68945390ed18d209c3c0e7
mingder wang <mingderwang@me.com>
2025-09-17 10:21:36 +0800

    add PeerId

--- relay.js
+++ relay.js
@@ -4,10 +4,17 @@ import { webSockets } from "@libp2p/websockets";
 import { noise } from "@chainsafe/libp2p-noise";
 import { yamux } from "@chainsafe/libp2p-yamux";
 import { circuitRelayServer } from "@libp2p/circuit-relay-v2";
+import { createEd25519PeerId } from "@libp2p/peer-id-factory";
+
+const main = async () => {
+const peerId = await createEd25519PeerId();
 
 const node = await createLibp2p({
+  peerId,
   addresses: {
-    listen: ["/ip4/0.0.0.0/tcp/15003/ws"],
+    listen: ["/ip4/0.0.0.0/tcp/15003/ws",
+//           "/dns4/myserver.com/tcp/443/wss"
+    ],
   },
   transports: [tcp(), webSockets()],
   connectionEncryption: [noise()],
@@ -18,3 +25,8 @@ const node = await createLibp2p({
 console.log("🚀 Relay node ready!");
 node.getMultiaddrs().forEach(ma => console.log(ma.toString()));
 
+console.log("✅ PeerId object:", node.peerId);
+console.log("✅ PeerId as string:", node.peerId.toString()); // 最常用
+}
+
+main()
