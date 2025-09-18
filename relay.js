383c00017c589ea898b56f8ba25d78ba80f77e96
mingder wang <mingderwang@me.com>
2025-09-18 14:18:39 +0800

    relay ok for client connect though this relay server

--- relay.js
+++ relay.js
@@ -1,23 +1,37 @@
-import { createLibp2p } from 'libp2p';
-import { noise } from '@chainsafe/libp2p-noise';
-import { yamux } from '@chainsafe/libp2p-yamux';
-import { tcp } from '@libp2p/tcp';
-import { circuitRelayServer } from '@libp2p/circuit-relay-v2';
-import { identify } from '@libp2p/identify';
+/* eslint-disable no-console */
 
-const node = await createLibp2p({
-  addresses: {
-    listen: ['/ip4/0.0.0.0/tcp/9001']
-  },
-  transports: [tcp()],
-  connectionEncryption: [noise()],
-  streamMuxers: [yamux()],
-  services: {
-    identify,
-    relay: circuitRelayServer() // Enables relay
-  }
-});
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
+import { identify } from '@libp2p/identify'
+import { webSockets } from '@libp2p/websockets'
+import { createLibp2p } from 'libp2p'
 
-await node.start();
-console.log('Relay Multiaddrs:', node.getMultiaddrs().map(ma => ma.toString()));
-// Output example: /ip4/127.0.0.1/tcp/9001/p2p/12D3KooW... (use this in clients)jk
+async function main () {
+  const node = await createLibp2p({
+    addresses: {
+      listen: ['/ip4/0.0.0.0/tcp/0/ws']
+      // TODO check "What is next?" section
+    },
+    transports: [
+      webSockets()
+    ],
+    connectionEncrypters: [
+      noise()
+    ],
+    streamMuxers: [
+      yamux()
+    ],
+    services: {
+      identify: identify(),
+      relay: circuitRelayServer()
+    }
+  })
+
+  console.log(`🚀🔭🛰📡🌈  Node started with id ${node.peerId.toString()}`)
+  console.log('Listening on:')
+  node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
+}
+
+main()
+
