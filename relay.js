3e6b4bde3464f5f7f2af66a1c24eb9b52ace1f85
mingder wang <mingderwang@me.com>
2025-09-18 10:17:43 +0800

    tmp, to compare with example

--- relay.js
+++ relay.js
@@ -1,32 +1,23 @@
-import { createLibp2p } from "libp2p";
-import { tcp } from "@libp2p/tcp";
-import { webSockets } from "@libp2p/websockets";
-import { noise } from "@chainsafe/libp2p-noise";
-import { yamux } from "@chainsafe/libp2p-yamux";
-import { circuitRelayServer } from "@libp2p/circuit-relay-v2";
-import { createEd25519PeerId } from "@libp2p/peer-id-factory";
-
-const main = async () => {
-const peerId = await createEd25519PeerId();
+import { createLibp2p } from 'libp2p';
+import { noise } from '@chainsafe/libp2p-noise';
+import { yamux } from '@chainsafe/libp2p-yamux';
+import { tcp } from '@libp2p/tcp';
+import { circuitRelayServer } from '@libp2p/circuit-relay-v2';
+import { identify } from '@libp2p/identify';
 
 const node = await createLibp2p({
-  peerId,
   addresses: {
-    listen: ["/ip4/0.0.0.0/tcp/15003/ws",
-//           "/dns4/myserver.com/tcp/443/wss"
-    ],
+    listen: ['/ip4/0.0.0.0/tcp/9001']
   },
-  transports: [tcp(), webSockets()],
+  transports: [tcp()],
   connectionEncryption: [noise()],
   streamMuxers: [yamux()],
-  relay: circuitRelayServer(),
+  services: {
+    identify,
+    relay: circuitRelayServer() // Enables relay
+  }
 });
 
-console.log("🚀 Relay node ready!");
-node.getMultiaddrs().forEach(ma => console.log(ma.toString()));
-
-console.log("✅ PeerId object:", node.peerId);
-console.log("✅ PeerId as string:", node.peerId.toString()); // 最常用
-}
-
-main()
+await node.start();
+console.log('Relay Multiaddrs:', node.getMultiaddrs().map(ma => ma.toString()));
+// Output example: /ip4/127.0.0.1/tcp/9001/p2p/12D3KooW... (use this in clients)jk
